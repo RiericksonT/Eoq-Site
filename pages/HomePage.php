dc03b317616849785dd52b5d9327b89840a49354
RiericksonT <riericksontorres@gmail.com>
2021-07-23 09:42:37 -0300

    V0.0.3

--- pages/HomePage.php
+++ pages/HomePage.php
@@ -17,14 +17,37 @@
       </div>
       <div class="HomeMenu"> 
         <button class="btn btn1" onclick="location.href='HomePage.php'">
-          <span/> a </span> 
+          <span style="padding-right:3px; padding-top: 8px; display:inline-block;">
+            <img class="manImg" src="../assets/images/home.png" alt="Pagina inicial"></img>
+          </span> 
+        </button>
+        <button class="btn btn2" onclick="location.href='Profile.php'">
+        <span style="padding-right:3px; padding-top: 8px; display:inline-block;">
+            <img class="profImage" src="../assets/images/profile.png" alt="Perfil"></img>
+          </span>  
+        </button>
+        <button class="btn btn3" onclick="location.href='Settings.php'">
+        <span style="padding-right:3px; padding-top: 8px; display:inline-block;">
+            <img class="setImg" src="../assets/images/settings.png" alt="Configuracoes"></img> 
+          </span>  
         </button>
       </div>
       <div class="HomeQuestions">
-        <h1> asd</h1>
+        <div class="yourAsk">
+          <input type="text" name="Ask" id="Ask" placeholder="Qual sua pergunta?">
+          <button class="sendAsk"> Perguntar </button>
+        </div>
+        <div class="anotherAsks">
+          <div class="askText">
+            <p>xxxxxxxxxxxxxxxxxxxx</p>
+          </div>
+          <a href="askPage.php">1 <img src="../assets/images/askBallon.png" alt=""></a>
+          <button class="sendReply" onclick="location.href='askPage.php'">Responder</button>
+        </div>
       </div>
       <div class="HomeBoard">
-        <h1> asd</h1>
+        <input type="text" name="search" id="search" placeholder="Procure uma pergunta?">
+        <button class="srcButton"><img src="../assets/images/lupa.png" alt=""></button>
       </div>
       <div id="footer">
         <?php require '../components/Footer.php';?>
