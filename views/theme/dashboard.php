f1c4405d2753b3f6d0cb3d0129ea5c82cbaa1950
DhyellTorres <torresadryel@gmail.com>
2021-08-02 04:46:22 -0300

    v0.0.6

--- views/theme/dashboard.php
+++ views/theme/dashboard.php
@@ -1,14 +1,24 @@
 <?php $this->layout("theme/_theme"); ?>
 
-<div class="page">
+<div id="main">
     <?php include dirname(__DIR__, 1) . "/components/Header.php"; ?>
-    <div>
-        <?php if ($user->photo) : ?>
-            <img class="page_user_photo" src="<?= $user->photo; ?>" alt="<?= $user->first_name; ?>" title="<?= $user->first_name; ?>" />
-        <?php endif; ?>
-        <h1>Olá <?= $user->first_name; ?>,</h1>
-        <p>Aqui é sua conta no projeto, mas por enquanto a única coisa que você pode fazer é sair dela :P</p>
-        <p><a class="btn btn-green" href="<?= $router->route("app.logout"); ?>" title="Sair agora">SAIR AGORA :)</a></p>
+    <div id="page">
+        <div id="profile">
+            <div id="photo">
+                <?php if ($user->photo) : ?>
+                    <img class="page_user_photo" src="<?= $user->photo; ?>" alt="<?= $user->first_name; ?>" title="<?= $user->first_name; ?>" />
+                <?php endif; ?>
+            </div>
+            <h3>Olá <?= $user->first_name; ?> <?= $user->last_name; ?>,</h3>
+            <p>Aqui é sua conta no projeto, mas por enquanto a única coisa que você pode fazer é sair dela :P</p>
+            <p><a class="btn btn-green" href="<?= $router->route("app.logout"); ?>" title="Sair agora">SAIR AGORA :)</a></p>
+        </div>
+        <div id="feed">
+            <h1>salve</h1>
+        </div>
+        <div id="relations">
+            <h1>salve</h1>
+        </div>
     </div>
     <?php include dirname(__DIR__, 1) . "/components/Footer.php"; ?>
 </div>
